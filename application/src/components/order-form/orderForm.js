ea481d421b10f85562f8df997a6e95e4a2e5fe4c
Tyler Whitsett <whitsett.tyler@gmail.com>
2020-04-09 17:59:44 -0700

    feat(css-style): bootstrapify order form

--- application/src/components/order-form/orderForm.js
+++ application/src/components/order-form/orderForm.js
@@ -51,28 +51,32 @@ class OrderForm extends Component {
             <Template>
                 <div className="form-wrapper">
                     <form>
-                        <label className="form-label">I'd like to order...</label><br />
-                        <select 
-                            value={this.state.order_item} 
-                            onChange={(event) => this.menuItemChosen(event)}
-                            className="menu-select"
-                        >
-                            <option value="" defaultValue disabled hidden>Lunch menu</option>
-                            <option value="Soup of the Day">Soup of the Day</option>
-                            <option value="Linguini With White Wine Sauce">Linguini With White Wine Sauce</option>
-                            <option value="Eggplant and Mushroom Panini">Eggplant and Mushroom Panini</option>
-                            <option value="Chili Con Carne">Chili Con Carne</option>
-                        </select><br />
-                        <label className="qty-label">Qty:</label>
-                        <select value={this.state.quantity} onChange={(event) => this.menuQuantityChosen(event)}>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                        </select>
-                        <button type="button" className="order-btn" onClick={(event) => this.submitOrder(event)}>Order It!</button>
+                        <div className="form-group">
+                            <label>I'd like to order...</label>
+                            <select 
+                                value={this.state.order_item} 
+                                onChange={(event) => this.menuItemChosen(event)}
+                                className="form-control"
+                            >
+                                <option value="" defaultValue disabled hidden>Lunch menu</option>
+                                <option value="Soup of the Day">Soup of the Day</option>
+                                <option value="Linguini With White Wine Sauce">Linguini With White Wine Sauce</option>
+                                <option value="Eggplant and Mushroom Panini">Eggplant and Mushroom Panini</option>
+                                <option value="Chili Con Carne">Chili Con Carne</option>
+                            </select>
+                        </div>
+                        <div className="form-group">
+                        <label>Qty:</label>
+                            <select className="form-control" value={this.state.quantity} onChange={(event) => this.menuQuantityChosen(event)}>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                            </select>
+                        </div>
+                        <button type="button" className="btn btn-success" onClick={(event) => this.submitOrder(event)}>Order It!</button>
                     </form>
                 </div>
             </Template>
