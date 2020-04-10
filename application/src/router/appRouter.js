d51f33df54d113d30c9bcf0802c766a87dda90f5
Tyler Whitsett <whitsett.tyler@gmail.com>
2020-04-10 10:21:18 -0700

    feat(route-guard): add and hook up guarded route component

--- application/src/router/appRouter.js
+++ application/src/router/appRouter.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Router, Route, Redirect } from "react-router-dom";
+import { connect } from 'react-redux';
+
 import { Main, Login, OrderForm, ViewOrders, EditOrder } from '../components';
+import RequireAuth from './requireAuth';
+import history from '../history';
+
+const mapStateToProps = (state) => {
+  return { auth: state.auth }
+}
 
 const AppRouter = (props) => {
   return (
-    <Router>
+    <Router history={history}>
       <Route path="/" exact component={Main} />
       <Route path="/login" exact component={Login} />
-      <Route path="/order" exact component={OrderForm} />
-      <Route path="/edit-order/:id" exact component={EditOrder} />
-      <Route path="/view-orders" exact component={ViewOrders} />
+      <RequireAuth path="/order" exact component={OrderForm} token={props.auth.token} />
+      <RequireAuth path="/edit-order/:id" exact component={EditOrder} token={props.auth.token} />
+      <RequireAuth path="/view-orders" exact component={ViewOrders} token={props.auth.token} />
     </Router>
   );
 }
 
-export default AppRouter;
+export default connect(mapStateToProps)(AppRouter);
